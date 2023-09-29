3947ed6cbd6abfad32f87ea8b97ff5ad4aae22cc
xxxtentacion <eleven980828@gmail.com>
2023-09-29 22:10:47 +0800

    chang

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const pxtoviewport = require('postcss-px-to-viewport');
 
 module.exports = {
   lintOnSave: false,
-  publicPath: './',
+  publicPath: '/ANTTOXAXA.github.io/',
   devServer: {
     // 关闭eslint语法验证
     //overlay: {
