89834a4d6f57a1a065da976c0593a145bb516d65
leo <eleven980828@gmail.com>
2024-06-20 01:29:45 +0800

    ani

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -16,17 +16,22 @@
         </ul>
       </div>
     </div>
-    <div class="NavMiddleBg"></div>
+
+    <div class="NavMid">
+      <div class="NavMiddleBg animate__animated animate__delay-1s" v-bind:class="{ 'animate__fadeInDown': isactive }">
+      </div>
+    </div>
+
     <div class="booking">
-      <div>
+      <!-- <div>
         <h1>BOOKING</h1>
         <p>1.Explain your idea</p>
         <p>2. Picture of the body part you're getting a tattoo </br>(lt helps me design the tattoo)</p>
         <p>3.Any special request in detail</p>
         <p>4.Confirm a date after the deposit is paid (1500rmb/hour)</p>
-      </div>
+      </div> -->
     </div>
-    <div class="foot">
+    <!-- <div class="foot">
       <div>
         <h1>预约</h1>
         <p>1.向我展示您需要纹身/遮盖的位置和大小</p>
@@ -39,10 +44,31 @@
           <img src="~@/assets/home/foot3.jpg" alt="">
         </div>
       </div>
+    </div> -->
+    <div class="secondSc">
+      <div class="bookimg">
+        <h1>BOOKING</h1>
+        <h1>预约</h1>
+        <p>1.Explain your idea</p>
+        <p>2. Picture of the body part you're getting a tattoo </br>(lt helps me design the tattoo)</p>
+        <p>3.Any special request in detail</p>
+        <p>4.Confirm a date after the deposit is paid (1500rmb/hour)</p>
+        <p>1.向我展示您需要纹身/遮盖的位置和大小</p>
+        <p>2.没有稿子提供，您所见全都是freehand完成,</br>您只需选一个我的作品，方便我明白您想要的</br> ins / 小红书 / 微博:daaamnj</p>
+        <p>3.支付一小时定金</br> (收费标准：1500rmb/h)</br> 定金不退</p>
+        <p>4.确定时间,有事不能来可提前沟通更改时间</p>
+        
+      </div>
+      <div class="footimg">
+        <img src="~@/assets/home/foot1.jpg" alt="">
+        <img src="~@/assets/home/foot2.jpg" alt="">
+        <img src="~@/assets/home/foot3.jpg" alt="">
+      </div>
     </div>
   </div>
 </template>
 <script>
+import 'animate.css'
 import { RouterLink } from 'vue-router';
 import Vue from 'vue'
 import { Popup } from 'vant';
@@ -52,11 +78,12 @@ export default {
   name: 'APP',
   data() {
     return {
-      show: false
+      show: false,
+      isactive: false
     };
   },
   mounted() {
-    
+    this.isactive = true
   },
   methods: {
 
@@ -67,110 +94,153 @@ export default {
 <style lang="less" scoped>
 @basecolor: white;
 @fontcolor: black;
-.main{
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to
+
+/* .fade-leave-active below version 2.1.8 */
+  {
+  opacity: 0;
+}
+
+.animate__animated.animate__animated {
+  --animate-duration: 2s;
+}
+
+:root {
+  --animate-duration: 2s;
+}
+
+.main {
   min-height: 100vh;
-  .titleLine{
-    background-color: black;
-    height: 20px;
-    margin-top: 20px;
-  }
-  .foot{
-    height: 600px;
-    margin-top: 100px;
-    // overflow: hidden;
-    background: url('~@/assets/home/footBg2.jpg') -230px 20px no-repeat,url('~@/assets/home/footBg1.jpg') 400px top no-repeat;
-    background-size: 600px 800px,600px 800px;
-    position: relative;
-    >div{
-      position: absolute;
-      left: 50%;
-      transform: translate(-50%);
-      top: -150px;
-      width: 16vh;
-      margin: 0 auto;
-      >div{
+  overflow: hidden;
+  display: flex;
+  padding-top: 20px;
+  flex-direction: column;
+
+  .secondSc {
+    height: 100vh;
+    background: url('~@/assets/home/footBg1.jpg') -230px 20px no-repeat, url('~@/assets/home/footBg2.jpg') 400px top no-repeat;
+    background-size: 600px 800px, 600px 800px;
+    padding-top: 500px;
+    .footimg {
         display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 50px;
-        img{
-          width: 110px;
+        justify-content: space-between;
+        width: 500px;
+        margin: 0 auto;
+
+        img {
+          width: 150px;
         }
       }
-      h1{
+    .bookimg {
+      width: 500px;
+      margin: 0 auto;
+      h1 {
         text-align: center;
         font-size: 22px;
         margin-bottom: 30px;
+        text-decoration: underline;
       }
-      p{
-          font-size: 16px;  
-          text-align: start;
-          font-weight: 600;
-          margin-bottom: 30px;
-        }
-    }
 
+      p {
+        font-size: 18px;
+        margin-left: 80px;
+        text-align: start;
+        font-weight: 600;
+        margin-bottom: 30px;
+
+        &:nth-of-type(4) {
+          margin-bottom: 100px;
+        }
+      }
+    }
   }
-  .NavBox{
+
+  .titleLine {
+    background-color: black;
+    height: 20px;
+  }
+
+  .NavBox {
     width: 90vw;
     margin: 0 auto;
-    
     display: flex;
     justify-content: space-between;
     margin-top: 70px;
-    .NavRight{
-      font-size: 22px;
+
+    .NavRight {
+      font-size: 20px;
       font-weight: 600;
-      ul{
+
+      ul {
         display: flex;
         flex-direction: column;
-        li{
+
+        li {
           margin-bottom: 10px;
         }
       }
     }
-    .NavLeft{
-      h1{
-        font-size: 22px;
+
+    .NavLeft {
+      h1 {
+        font-size: 20px;
         font-weight: 600;
       }
-      p{
+
+      p {
         margin-top: 20px;
         font-size: 16px;
         text-align: start;
       }
     }
   }
-  .NavMiddleBg{
-    background: url('~@/assets/home/NAV.jpg') no-repeat center center;
-    background-size: cover;
+
+  .NavMid {
     margin: 0 auto;
-    width: 300px;
-    margin-top: -50px;
-    height: 300px;
+    width: 250px;
+    height: 350px;
+    position: relative;
   }
-  .booking{
+
+  .NavMiddleBg {
+    background: url('~@/assets/home/NAV.png') no-repeat center center;
+    background-size: cover;
+    width: 250px;
+    height: 250px;
+    position: absolute;
+    top: 100px;
+  }
+
+  .booking {
     width: 100%;
-    height: 600px;
+    height: 1050px;
     margin-top: -4px;
-    background: url('~@/assets/home/midBg1.jpg') left top no-repeat,url('~@/assets/home/midBg2.jpg') right top no-repeat;
-    background-size: 20vh 700px,20vh 700px;
-    padding-top: 100px;
-    >div{
+    background: url('~@/assets/home/midbg1.png') -170px -140px no-repeat, url('~@/assets/home/midbg2.png') 545px -140px no-repeat, url('~@/assets/home/midbg4.png') right 1200px no-repeat, url('~@/assets/home/midbg3.png') right 1200px no-repeat;
+    background-size: 50vw 1000px, 50vw 1000px;
+
+    >div {
       width: 13vh;
       margin: 0 auto;
-      h1{
+
+      h1 {
         text-align: center;
         font-size: 22px;
         margin-bottom: 30px;
       }
-      p{
-          font-size: 18px;  
-          text-align: start;
-          margin-bottom: 30px;
-        }
+
+      p {
+        font-size: 18px;
+        text-align: start;
+        margin-bottom: 30px;
+      }
     }
   }
 }
-
 </style>
